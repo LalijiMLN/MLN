7d4bc74008c55750311995ad183a364c3cfa6578
Rahul0272 <105348416+Rahul0272@users.noreply.github.com>
2024-04-28 10:18:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,9 +27,9 @@ API_HASH = environ.get("API_HASH", "88cf2c17c7a2bd21f4204c89c648dd40")
 ADMINS = int(environ.get("ADMINS", "1557042262 6482751939 6153802459"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://iitjeerahul2019:Pdei1shCmiUBjxEq@cluster0.kwqjdx4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://RAS_TEAM:rasteam@123@cluster0.619qk8h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "mongodb+srv://gakavif673:5ZLlRcFTysm0XKlo@cluster0.rl38jcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://yadavrahul2003:IbRdr2y3Qs5juADm@cluster0.8epud6d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "vjbotz")
 
 
@@ -39,12 +39,12 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "RAS_Store_Bot") # your bot username 
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/66604e5911df5a0c43ffa.jpg https://te.legra.ph/file/66604e5911df5a0c43ffa.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "10")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "600")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002055918522"))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002131984492')).split()]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001923834324"))
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1002128363054')).split()]
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
@@ -65,8 +65,8 @@ class Var(object):
     MULTI_CLIENT = False
     name = str(getenv('name', 'filetolinkvjbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002003233324'))
+    WORKERS = int(getenv('WORKERS', '3'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002141038542'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -81,9 +81,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://ras-file-store-bot.onrender.com"
+        URL = ""
     else:
-        URL = "https://ras-file-store-bot.onrender.com"
+        URL = ""
 
 
 
